e6f37f9ed2ddabc3252e26cab58c2a5f4bd3d7be
DD1C74707 <gracevalley101@gmail.com>
2023-06-21 22:34:17 -0700

    Fix unknown instruction bug

--- file_processor.c
+++ file_processor.c
@@ -95,7 +95,7 @@ void process_instructions(info_t *info)
 		{NULL, NULL}
 	};
 
-	while (info->line_token[0] && instructions[i].opcode != NULL)
+	while (info->line_tokens[0] && instructions[i].opcode != NULL)
 	{
 		if (strcmp(instructions[i].opcode, info->line_tokens[0]) == 0)
 		{
