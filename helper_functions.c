311a1f0dff44efb3f129d210bbfcdb2e37e0c3a6
DD1C74707 <gracevalley101@gmail.com>
2023-06-22 02:02:00 -0700

    Fix is digit to handle negatives

--- helper_functions.c
+++ helper_functions.c
@@ -37,7 +37,12 @@ int _isdigit(const char *s)
 {
 	unsigned int i;
 
-	for (i = 0; s[i]; i++)
+	if (s[0] == '-')
+		i = 1;
+	else
+		i = 0;
+
+	for (; s[i]; i++)
 	{
 		if (s[i] < 48 || s[i] > 57)
 			return (0);
